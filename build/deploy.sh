2d66965b1e14d988f1b75b4914d8229e21c16e1c
bravin <bravind91@google.com>
2025-04-30 16:07:07 +0000

    Initial dockerized version

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Pull the latest image from Docker Hub (replace your image name and repo)
-docker pull <your-dockerhub-username>/my-react-app:latest
-
-# Stop and remove any running containers
-docker stop react-app-container
-docker rm react-app-container
-
-# Run the new container
-docker run -d --name react-app-container -p 80:80 <your-dockerhub-username>/my-react-app:latest
-
-# Check if the container started successfully
-if [ $? -eq 0 ]; then
-  echo "Deployment successful!"
-else
-  echo "Error: Deployment failed"
-  exit 1
-fi
+docker-compose down
+docker-compose up -d --build
